2809bbaa76db7fc3d2a6f648ad46496d91094185
Bohdan Shchur <bodjashchur@gmail.com>
2021-10-26 12:40:17 +0300

    adaptive version

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const burgerBtn = document.querySelector(".burger");
 const dropDownMenu = document.querySelector(".menu-items");
+const img =  document.querySelector(".img-form");
 
 
 const burgerDropDown = () => {
-    dropDownMenu.style.display = dropDownMenu.style.display === "block" ? "none" : "block";
+    // dropDownMenu.className = dropDownMenu.className === "menu-items" ? " responsive" : "menu-items";
+    if(dropDownMenu.className === "menu-items") {
+        dropDownMenu.className += " responsive";
+    } else {
+        dropDownMenu.className = "menu-items";
+    }
 }
 
-burgerBtn.addEventListener('click', burgerDropDown, false);
+const addImageForm = () => {
+
+}
+
+const input = img.childNodes[1];
+burgerBtn.addEventListener('click', burgerDropDown);
